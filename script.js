03070244fdf361902d5d0158e1d1005973ed995b
AaleandroG <alejandroiatiqueteinporta@gmail.com>
2026-03-05 21:20:34 +0100

    Remove unused functions from script.js
    Removed unused QR generator, timer, word counter, and link shortener functions.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// QR GENERATOR
-
 function generateQR(){
 
 let text=document.getElementById("qr-text").value
@@ -26,9 +24,6 @@ a.click()
 
 }
 
-
-// TIMER
-
 let timer
 let timeLeft=0
 let totalTime=0
@@ -53,10 +48,6 @@ function updateTimer(){
 if(timeLeft<=0){
 
 clearInterval(timer)
-
-let audio=new Audio("https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg")
-
-audio.play()
 
 return
 
@@ -95,23 +86,6 @@ document.getElementById("progress").style.width="0%"
 
 }
 
-function addTime(){
-
-timeLeft+=10
-
-}
-
-function removeTime(){
-
-timeLeft-=10
-
-if(timeLeft<0)timeLeft=0
-
-}
-
-
-// WORD COUNTER
-
 document.getElementById("text-input").addEventListener("input",function(){
 
 let text=this.value
@@ -122,25 +96,4 @@ document.getElementById("words").innerText=words.length
 
 document.getElementById("chars").innerText=text.length
 
-document.getElementById("charsNoSpace").innerText=text.replace(/\s/g,"").length
-
-document.getElementById("reading").innerText=Math.ceil(words.length/200)
-
 })
-
-
-// LINK SHORTENER
-
-function shortLink(){
-
-let url=document.getElementById("long-url").value
-
-fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-.then(res=>res.text())
-.then(data=>{
-
-document.getElementById("short-result").innerHTML=`<a href="${data}" target="_blank">${data}</a>`
-
-})
-
-}
